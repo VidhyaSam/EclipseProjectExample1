28a06676a7b725802aab1f39ca69521b9b0d16e0
Vidhya <meenakshi.sambandan@gmail.com>
2016-06-02 10:46:00 -0700

    Uploading Eclipse Project Example 1 to Git

---
+++ ProjectExample1/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author Meenakshi S
+ *
+ */
+class HelloWorld{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World! This is my first java application");
+	}
+}
